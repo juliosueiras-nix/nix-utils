484ffb709d32cfbf1c93df33fca5ce77f9a399da
Tom Bereknyei <tomberek@gmail.com>
2021-08-18 17:03:43 -0400

    bundler

--- flake.nix
+++ flake.nix
@@ -2,17 +2,42 @@
   description = "Nix-Utils, nix utilities";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/3b6c3bee9174dfe56fd0e586449457467abe7116";
+    nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+  (flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
   let
     pkgs = import nixpkgs {
       inherit system;
     };
   in {
     rpmDebUtils = pkgs.callPackage ./utils/rpm-deb {};
-  });
+
+  })) // {
+    bundlers =  with builtins; let
+          pkgs = system: import nixpkgs {
+            inherit system;
+          };
+          # Function requires a derivation along with it's meta data instead
+          # of a built store path provided by `nix bundle`
+          package = program: system: let
+            name= (parseDrvName ( elemAt (split "/[0-9a-df-np-sv-z]{32}-" (program)) 2)).name;
+            in (pkgs system).runCommand name {} ''
+             mkdir -p $out/bin
+             ln -s ${program} $out/bin/.
+          '';
+          utils = system: (pkgs system).callPackage ./utils/rpm-deb {};
+             in
+    {
+      rpm = { program, system }: 
+          (utils system).buildFakeSingleRPM (package program system);
+
+      deb = { program, system }: 
+          (utils system).buildFakeSingleDeb (package program system);
+
+    };
+    defaultBundler = self.bundlers.rpm;
+  };
 }
